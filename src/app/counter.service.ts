88374ede445908c91e3de4ef26d7818186ddef7f
jayme.weiss <jayme.weiss@entelect.co.za>
2020-11-17 16:03:52 +0200

    users service and counter service

--- src/app/counter.service.ts
+++ src/app/counter.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CounterService {
+  activeToInactive = 0;
+  inactiveToActive = 0;
 
-  constructor() { }
+  constructor() {
+  }
+
+  increaseActiveToInactiveCounter(): void {
+    this.activeToInactive += 1;
+    console.log(`Active to Inactive counter: ${this.activeToInactive}`);
+  }
+
+  increaseInactiveToActiveCounter(): void {
+    this.inactiveToActive += 1;
+    console.log(`Inactive to active counter: ${this.inactiveToActive}`);
+  }
 }
--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {CounterService} from './counter.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  activeUsers = ['Max', 'Anna'];
+  inactiveUsers = ['Chris', 'Manu'];
+
+  setUserToInactive(id: number) {
+    this.inactiveUsers.push(this.activeUsers[id]);
+    this.activeUsers.splice(id, 1);
+    this.counterService.increaseActiveToInactiveCounter();
+  }
+
+  setUserToActive(id: number) {
+    this.activeUsers.push(this.inactiveUsers[id]);
+    this.inactiveUsers.splice(id, 1);
+    this.counterService.increaseInactiveToActiveCounter();
+  }
+
+  constructor(private counterService: CounterService) {
+  }
 }
